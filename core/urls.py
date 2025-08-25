307bccc5ec2beeea4ba8fca7af79f5d4d92426d6
Paras105 <paraspotnurwar@gmail.com>
2025-08-25 14:37:11 +0530

    SAFE WALA COMMIT - REMOVED COLLECT PHONE

--- core/urls.py
+++ core/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('orders.urls')),
     path('auth/', include('social_django.urls', namespace='social')),
+    
     path('logout/', LogoutView.as_view(next_page='/'), name='custom_logout'),
    ]
